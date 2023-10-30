10f6ccdfaeb131473f99264d40680c184f8e9c3f
alireza.rahnama <alirezarm3@gmail.com>
2023-10-30 18:46:27 -0230

    Added new project, implemeting full authentication and transitioned to cloud database and storage.

--- src/components/Data.js
+++ src/components/Data.js
@@ -11,12 +11,19 @@ import portfoliov1 from '../projects-images-videos/images/portfoliov1.png'
 import portfoliov2 from '../projects-images-videos/images/portfoliov2.png'
 import rio from '../projects-images-videos/images/janiro.gif'
 import diary from '../projects-images-videos/images/dear-diary.gif'
+import diaryWithFirebase from '../projects-images-videos/images/dear-diary.gif'
 
 const data = [
     {
+        source: diaryWithFirebase,
+        name:"Dear Diary With Authentication and DataBase Integration",
+        desc: ": Developed a cross-platform mobile app implementing full authentication and enable users to persist diary entries in NoSQL format in Firestore database, search/filter entreis by keyword and/or by month, upload images from phone to cloud Storage, and export entries to pdf file leveraging Dart, Flutter, Firebase Fire Store and Authentication. Hover over image to checkout the github repo.",
+        git_url: "https://github.com/Alireza-rahnama/dear_diary_flutter_hive_app"
+    },
+    {
         source: diary,
         name:"Dear Diary",
-        desc: ": Developed a cross-platform mobile app allowing users to persist data and perform CRUD operations, filter entries by month, and export entries to pdf file leveraging Dart, Flutter, and Hive. Hover over image to checkout the github repo.",
+        desc: ": Developed a cross-platform mobile app allowing users to persist data and perform CRUD operations, filter entries by month, and export entries to pdf file locally leveraging Dart, Flutter, and Hive. Hover over image to checkout the github repo.",
         git_url: "https://github.com/Alireza-rahnama/dear_diary_flutter_hive_app"
     },
     {
